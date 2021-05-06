9ea66506a28f3c91747f80984a05fd60a3c9c428
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-05-05 21:29:35 -0300

    Add reviewDAO and controller

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -1,22 +1,22 @@
-import RestaurantsDAO from "../dao/restaurantsDAO"
+import RestaurantsDAO from "../dao/restaurantsDAO.js"
 
 export default class RestaurantsController {
-    static async apiGetRestaurants(req, res, next){
+    static async apiGetRestaurants(req, res, next) {
         //To set parameters from the query string if they exist or not. Parameters are: restaurantsPerPage, page and filters
         const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.restaurantsPerPage, 10) : 20
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
         let filters = {}
-        if(req.query.cuisine){
+        if (req.query.cuisine) {
             filters.cuisine = req.query.cuisine
         }
-        else if(req.query.zipcode){
+        else if (req.query.zipcode) {
             filters.zipcode = req.query.zipcode
         }
-        else if(req.query.name){
+        else if (req.query.name) {
             filters.name = req.query.name
         }
 
-        const {restaurantsList, totalNumRestaurants} = await RestaurantsDAO.getRestaurants({
+        const { restaurantsList, totalNumRestaurants } = await RestaurantsDAO.getRestaurants({
             filters,
             page,
             restaurantsPerPage,
--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,9 +1,16 @@
 import express from "express"
 import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviewsCtrl from "./review.controller.js"
 
 const router = express.Router()
 
 //router.route("/").get((req, res) => res.send("Kon'nichiwa sekai"))
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+
+router
+    .route("/review")
+    .post(ReviewsCtrl.apiPostReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
