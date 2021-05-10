fcff264164b6b56f5a836f38f4e433e9696afba3
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-05-10 20:22:37 -0300

    Add get by id and cuisine routes

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,5 +1,9 @@
 //DAO: Data Access Object
 
+//import { ObjectID } from "bson"
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectID
+
 let restaurants //To store a reference to our database
 
 export default class RestaurantsDAO {
@@ -19,7 +23,7 @@ export default class RestaurantsDAO {
         filters = null, //To sort things
         page = 0,
         restaurantsPerPage = 20,
-    } = {}) {
+        } = {}) {
 
         let query
         if (filters) {
@@ -40,20 +44,75 @@ export default class RestaurantsDAO {
             cursor = await restaurants.find(query) //This will find all the restaurants form the db with the query we pass in
         } catch (e) {
             console.error(`Unable to issue find command, ${e}`)
-            return {restaurantsList: [], totalNumRestaurants: 0}
+            return { restaurantsList: [], totalNumRestaurants: 0 }
         }
 
-        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage*page)
+        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
 
         try {
             const restaurantsList = await displayCursor.toArray()
             //const totalNumRestaurants = page === 0 ? await restaurants.countDocuments(query) : 0
             const totalNumRestaurants = await restaurants.countDocuments(query)
 
-            return {restaurantsList, totalNumRestaurants}
+            return { restaurantsList, totalNumRestaurants }
         } catch (e) {
             console.error(`Unable to convert cursor to array or problem counting documents, ${e}`)
-            return {restaurantsList: [], totalNumRestaurants: 0}
+            return { restaurantsList: [], totalNumRestaurants: 0 }
+        }
+    }
+
+    static async getRestaurantById(id){
+        try {
+            const pipeline = [
+                    {
+                        $match: {
+                            _id: new ObjectId(id),
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: "reviews",
+                            let: {
+                                id: "_id",
+                            },
+                            pipeline: [
+                                {
+                                    $match: {
+                                        $expr: {
+                                            $eq: ["$restaurant_id", "$$id"],
+                                        },
+                                    },
+                                },
+                                {
+                                    $sort: {
+                                        date: -1,
+                                    },
+                                },
+                            ],
+                            as: "reviews",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            reviews: "$reviews",
+                        },
+                    },
+            ]
+            return await restaurants.aggregate(pipeline).next()
+        } catch (e) {
+            console.error(`Something went wrong in getRestaurantById: ${e}`)
+            throw e
+        }
+    }
+
+    static async getCuisines(){
+        let cuisines = []
+        try {
+            cuisines = await restaurants.distinct("cuisine")
+            return cuisines
+        } catch (e) {
+            console.error(`Unable to get cuisines: ${e}`)
+            return cuisines
         }
     }
 }
